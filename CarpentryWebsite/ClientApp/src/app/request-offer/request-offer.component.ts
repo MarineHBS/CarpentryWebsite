cc0dba80e3c37199477c00e6a3fdbfff5ae7a5d4
Máté Szlovák <marinehbs@gmail.com>
2019-10-02 21:08:07 +0200

    Requesting offer works

--- CarpentryWebsite/ClientApp/src/app/request-offer/request-offer.component.ts
+++ CarpentryWebsite/ClientApp/src/app/request-offer/request-offer.component.ts
@@ -9,7 +9,7 @@ import { PriceEstimateComponent } from '../price-estimate/price-estimate.compone
 export class RequestOfferComponent implements OnInit, AfterContentInit {
 
   @ViewChild('container', {read: ViewContainerRef}) container;
-  priceEstimateValue = 0;
+  values = new Map();
 
   constructor(private resolver: ComponentFactoryResolver) { }
 
@@ -17,8 +17,10 @@ export class RequestOfferComponent implements OnInit, AfterContentInit {
     this.addNewComponent();
   }
 
-  getPriceEstimateValue(value: number) {
-    this.priceEstimateValue += value;
+  get priceEstimateValue(): number {
+    let priceEstimateValue = 0;
+    this.values.forEach(v => priceEstimateValue += v);
+    return priceEstimateValue;
   }
 
   ngAfterContentInit(): void {
@@ -27,8 +29,9 @@ export class RequestOfferComponent implements OnInit, AfterContentInit {
   addNewComponent() {
     const priceEst = this.resolver.resolveComponentFactory(PriceEstimateComponent);
     const priceEstComponent = this.container.createComponent(priceEst);
-    priceEstComponent.instance.priceEstimateResultEvent.subscribe(value => this.priceEstimateValue += value);
-    priceEstComponent.instance.needNewComponentEvent.subscribe(event => this.addNewComponent());
+    priceEstComponent.instance.priceEstimateResultEvent.subscribe(value => this.values.set(priceEstComponent, value));
+    priceEstComponent.instance.needNewComponentEvent.subscribe(() => this.addNewComponent());
+
   }
 
 }
